a506dc983af1f6cfefe383e8845f2400281b3c31
Brett Salyer <brettsalyer@gmail.com>
2020-07-02 15:52:34 -0400

    Added LiveStream class for handling the launching of live streams

---
+++ lib/screens/services/livestream.dart
@@ -0,0 +1,26 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+
+
+class LiveStream { 
+
+  final youtubeURL = "https://www.youtube.com/embed/live_stream?channel=UC6qjNu84xNJXte6jGW3Or-g";
+
+  void launchLivestreamYoutube() async{
+
+    if (await canLaunch(youtubeURL)) {
+      await launch(youtubeURL);
+    } else {
+      throw 'Could not launch $youtubeURL';
+    }
+
+  }
+
+  void launchLiveStreamFacebook(){
+
+  }
+
+  void launchLivestreamChurchOnline(){
+
+  }
+}
