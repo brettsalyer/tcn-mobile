56375ee6a60f4730a6465cdbf5a478fb1b5f5eed
Brett Salyer <brettsalyer@gmail.com>
2020-07-02 15:52:57 -0400

    Filled out service information and provided links to view them live

--- lib/screens/services/services.dart
+++ lib/screens/services/services.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:TCN/tcn_widgets.dart';
 import 'package:TCN/screens/services/components/tcn_decorations.dart';
-
+import 'package:TCN/screens/services/livestream.dart';
 
 // Page not reachable from the navigation drawer. It is stateless, and can be left by using the 
 // Navigation drawer.
@@ -15,35 +15,129 @@ class ServicesPage extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       backgroundColor: Color.fromRGBO(130, 130, 130, 100),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          pageHeader,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: "Roboto",
+            fontWeight: FontWeight.w100,
+            fontSize: 28,
+          ),
+        ),
       ),
       drawer: NavDrawer(),
       body: Center(
         child: Column (
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              padding: EdgeInsetsDirectional.only(bottom: 100, top: 40),
-              child: Text(
-                pageHeader, 
-                style: TextStyle(
-                  fontFamily: "Roboto", 
-                  fontWeight: FontWeight.w300, 
-                  fontSize: 28, 
-                  color: Colors.white
-                ),
-              )
-            ),
             Flexible(
               child: FractionallySizedBox(
                 widthFactor: 0.95,
-                heightFactor: 1,
+                heightFactor: 0.8,
                 child: Container (
                   decoration: TCNDecorations().servicesDecoration(),
-                  child: Text("Sunday Morning", textAlign: TextAlign.center),
+                  padding: EdgeInsets.only(top: 25, bottom: 25,),
+                  child: Column(
+                    children: <Widget>[
+                      
+                      Padding(
+                        padding: EdgeInsets.only(top: 25, bottom:20),
+                        child: Text(
+                          "Sunday Morning", 
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:10),
+                        child: Text(
+                          "Worship begins at 10:40 AM", 
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:10),
+                        child: Text(
+                          "Sermon Immediately Following", 
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:50),
+                        child: FlatButton(
+                          child: Text("View Live", style: TextStyle(color: Colors.white,),),
+                          color: Colors.blue,
+                          // Use the URL Plugin to launch the stream through YouTube (if desired)
+                          onPressed: () {
+                            LiveStream().launchLivestreamYoutube();
+                          }
+                        )
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(top: 25, bottom:20),
+                        child: Text(
+                          "Sunday Evening", 
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:10),
+                        child: Text(
+                          "Worship begins at 6:00 PM", 
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:10),
+                        child: Text(
+                          "Sermon Immediately Following", 
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom:40),
+                        child: FlatButton(
+                          child: Text("View Live", style: TextStyle(color: Colors.white,),),
+                          color: Colors.blue,
+                          // Use the URL Plugin to launch the stream through YouTube (if desired)
+                          onPressed: () {
+                            LiveStream().launchLivestreamYoutube();
+                          },
+                        )
+                      ),
+
+                    ],
+                  )
                 )
               ),
             )
--- lib/tcn_widgets.dart
+++ lib/tcn_widgets.dart
@@ -4,48 +4,50 @@ import 'package:flutter/material.dart';
 class NavDrawer extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-      return Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              height: 150,
-              child: DrawerHeader(
-                child: Text(""),
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  image: DecorationImage(
-                    image: ExactAssetImage('images/drawer_tcn_logo_black.png'),
+      return SafeArea(
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 150,
+                child: DrawerHeader(
+                  child: Text(""),
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    image: DecorationImage(
+                      image: ExactAssetImage('images/drawer_tcn_logo_black.png'),
+                    ),
                   ),
                 ),
+              ), 
+              ListTile(
+                title: Text("Home"),
+                onTap: () {
+                  Navigator.pushNamed(context, '/');
+                },
               ),
-            ), 
-            ListTile(
-              title: Text("Home"),
-              onTap: () {
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-            ListTile(
-              title: Text("News & Announcements"),
-            ),
-            ListTile(
-              title: Text("Digital Sermons"),
-            ),
-            ListTile(
-              title: Text("Request Prayer"),
-            ),
-            ListTile(
-              title: Text("Contact"),
-            ),
-            ListTile(
-              title: Text("Online Giving"),
-            ),
-            ListTile(
-              title: Text("Sunday Bulletin"),
-            ),
-          ],
-        ),
-      );
+              ListTile(
+                title: Text("News & Announcements"),
+              ),
+              ListTile(
+                title: Text("Digital Sermons"),
+              ),
+              ListTile(
+                title: Text("Request Prayer"),
+              ),
+              ListTile(
+                title: Text("Contact"),
+              ),
+              ListTile(
+                title: Text("Online Giving"),
+              ),
+              ListTile(
+                title: Text("Sunday Bulletin"),
+              ),
+            ],
+          ),
+        )
+      );   
   }
 }
